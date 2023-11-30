193826ebd2c51ba6f1e929f25abd584018813c45
ebpf <bordeianu.stefano@gmai.com>
2023-11-30 14:30:55 +0100

    p

--- benchmarking/test/socket_standard_test.sh
+++ benchmarking/test/socket_standard_test.sh
@@ -5,11 +5,11 @@ do
     printf "s-%s\n" $a >> waitServerResults.txt
     for y in {1..10}
     do
-        .udpServer/standard/p -p 2020 -d 115 -s $a -b 1
+        ./udpServer/standard/p -p 2020 -d 115 -s $a -b 1
         sleep 5
-        .socket/p 2020 115
+        ./socket/server 2020 115
         sleep 5
-        .udpServer/submitAndWait/p -p 2020 -d 115 -s $a -w 1
+        ./udpServer/submitAndWait/p -p 2020 -d 115 -s $a -w 1
         sleep 5
     done
     sleep 300
