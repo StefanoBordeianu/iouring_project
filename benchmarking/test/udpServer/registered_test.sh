81a7b5f9096cfab6be45d2c7b28874fe0384d9a8
ebpf <bordeianu.stefano@gmai.com>
2023-12-12 09:41:43 +0100

    p

--- benchmarking/test/udpServer/registered_test.sh
+++ benchmarking/test/udpServer/registered_test.sh
@@ -1,19 +1,38 @@
 #!/bin/bash
-for a in 20 50 100 250 500 1000 1450
+#for a in 20 50 100 250 500 1000 1450
+#do
+#    printf "s-%s\n" $a >> standardRegisteredServerResults.txt
+#    printf "s-%s\n" $a >> waitRegisteredServerResults.txt
+#    for x in 1 10 30 60 100 250 500 1000
+#    do
+#        printf "b-%s\n" $x >> standardRegisteredServerResults.txt
+#        printf "w-%s\n" $x >> waitRegisteredServerResults.txt
+#        for y in {1..10}
+#        do
+#            ./standard/r -p 2020 -d 115 -s $a -b $x
+#            sleep 5
+#            ./submitAndWait/r -p 2020 -d 115 -s $a -w $x
+#            sleep 5
+#        done
+#    done
+#    sleep 300;
+#done
+
+for a in 20 50 100 250 500 1000
 do
     printf "s-%s\n" $a >> standardRegisteredServerResults.txt
     printf "s-%s\n" $a >> waitRegisteredServerResults.txt
     for x in 1 10 30 60 100 250 500 1000
     do
-        printf "b-%s\n" $x >> standardServerResults.txt
-        printf "w-%s\n" $x >> waitServerResults.txt
-        for y in {1..10}
+        printf "b-%s\n" $x >> standardRegisteredServerResults.txt
+        printf "w-%s\n" $x >> waitRegisteredServerResults.txt
+        for y in {1..2}
         do
-            ./standard/r -p 2020 -d 115 -s $a -b $x
-            sleep 5
-            ./submitAndWait/r -p 2020 -d 115 -s $a -w $x
-            sleep 5
+            ./standard/r -p 2020 -d 12 -s $a -b $x
+            sleep 3
+            ./submitAndWait/r -p 2020 -d 12 -s $a -w $x
+            sleep 3
         done
     done
-    sleep 300;
+    sleep 15;
 done
