746107ccb42a8dc1ac1fc08db09c52b0ba837dfc
ebpf <bordeianu.stefano@gmai.com>
2024-03-16 11:27:25 +0100

    PEEPO

--- benchmarking/io_uring/standard/standard_batching.c
+++ benchmarking/io_uring/standard/standard_batching.c
@@ -31,125 +31,125 @@ struct args{
 struct args args;
 
 void freemsg(struct msghdr * msg){
-    free(msg->msg_iov->iov_base);
-    free(msg->msg_iov);
-    free(msg);
+      free(msg->msg_iov->iov_base);
+      free(msg->msg_iov);
+      free(msg);
 }
 
 void parseArgs(int argc, char* argv[]){
-    int opt;
-    args.port = 2020;
-    args.batching = 1;
-    args.duration = 10;
-    args.test = 0;
+      int opt;
+      args.port = 2020;
+      args.batching = 1;
+      args.duration = 10;
+      args.test = 0;
 
-    while((opt =getopt(argc,argv,"hs:p:d:b:t")) != -1) {
-        switch (opt) {
-            case 'p':
-                args.port = atoi(optarg);
-                break;
-            case 'b':
-                args.batching =  atoi(optarg);
-                break;
-            case 'd':
-                args.duration = atoi(optarg);
-                break;
-            case 's':
-                args.size = atoi(optarg);
-                break;
-              case 't':
-                    args.test = 1;
-                    break;
-        }
-    }
+      while((opt =getopt(argc,argv,"hs:p:d:b:t")) != -1) {
+            switch (opt) {
+                  case 'p':
+                        args.port = atoi(optarg);
+                        break;
+                  case 'b':
+                        args.batching =  atoi(optarg);
+                        break;
+                  case 'd':
+                        args.duration = atoi(optarg);
+                        break;
+                  case 's':
+                        args.size = atoi(optarg);
+                        break;
+                  case 't':
+                        args.test = 1;
+                        break;
+            }
+      }
 }
 
 
 int openListeningSocket(int port){
-    int socketfd;
-    int opt = 1;
-    struct sockaddr_in add;
+      int socketfd;
+      int opt = 1;
+      struct sockaddr_in add;
 
-    socketfd = socket(AF_INET, SOCK_DGRAM, 0);
-    if(socketfd < 0){
-        printf("SERVER: Error while creating the socket\n");
-        exit(-1);
-    }
-    if(setsockopt(socketfd,SOL_SOCKET,SO_REUSEADDR|SO_REUSEPORT,
-                       &opt,sizeof (opt))){
-        printf("SERVER: Socket options error\n");
-        exit(-1);
-    }
+      socketfd = socket(AF_INET, SOCK_DGRAM, 0);
+      if(socketfd < 0){
+            printf("SERVER: Error while creating the socket\n");
+            exit(-1);
+      }
+      if(setsockopt(socketfd,SOL_SOCKET,SO_REUSEADDR|SO_REUSEPORT,
+                    &opt,sizeof (opt))){
+            printf("SERVER: Socket options error\n");
+            exit(-1);
+      }
 
-    add.sin_port = htons(port);
-    add.sin_family = AF_INET;
-    add.sin_addr.s_addr = INADDR_ANY;
-    if(bind(socketfd,(struct sockaddr *)&add, sizeof(add)) < 0){
-        perror("bind()");
-        exit(-1);
-    }
-    return socketfd;
+      add.sin_port = htons(port);
+      add.sin_family = AF_INET;
+      add.sin_addr.s_addr = INADDR_ANY;
+      if(bind(socketfd,(struct sockaddr *)&add, sizeof(add)) < 0){
+            perror("bind()");
+            exit(-1);
+      }
+      return socketfd;
 }
 
 int add_recv_request(int socket, long readlength){
-    struct io_uring_sqe* sqe = io_uring_get_sqe(&ring);
-    struct request* req = malloc(sizeof(struct request));
+      struct io_uring_sqe* sqe = io_uring_get_sqe(&ring);
+      struct request* req = malloc(sizeof(struct request));
 
-    struct msghdr* msg = malloc(sizeof(struct msghdr));
-    struct iovec* iov = malloc(sizeof(struct iovec));
+      struct msghdr* msg = malloc(sizeof(struct msghdr));
+      struct iovec* iov = malloc(sizeof(struct iovec));
 
-    memset(msg, 0, sizeof(struct msghdr));
-    memset(iov,0,sizeof(struct iovec));
-    iov->iov_base = malloc(readlength);
-    iov->iov_len = readlength;
-    msg->msg_name = NULL;
-    msg->msg_namelen = 0;
-    msg->msg_iov = iov;
-    msg->msg_iovlen = 1;
+      memset(msg, 0, sizeof(struct msghdr));
+      memset(iov,0,sizeof(struct iovec));
+      iov->iov_base = malloc(readlength);
+      iov->iov_len = readlength;
+      msg->msg_name = NULL;
+      msg->msg_namelen = 0;
+      msg->msg_iov = iov;
+      msg->msg_iovlen = 1;
 
-    req->type = EVENT_TYPE_RECV;
-    req->message = msg;
-    io_uring_prep_recvmsg(sqe,socket, msg,0);
-    io_uring_sqe_set_data(sqe, req);
-    return 1;
+      req->type = EVENT_TYPE_RECV;
+      req->message = msg;
+      io_uring_prep_recvmsg(sqe,socket, msg,0);
+      io_uring_sqe_set_data(sqe, req);
+      return 1;
 }
 
 void startBatchingServer(int socketfd){
-    struct io_uring_cqe* cqe [args.batching];
-    int start = 0;
-    unsigned int packets_rec;
-    int rec;
+      struct io_uring_cqe* cqe [args.batching];
+      int start = 0;
+      unsigned int packets_rec;
+      int rec;
 
-    for(int i=0;i<args.batching;i++)
-        add_recv_request(socketfd,args.size);
-    io_uring_submit(&ring);
-
-    printf("Entering server loop\n");
-    while (1) {
-        packets_rec = io_uring_peek_batch_cqe(&ring, cqe, args.batching);
-        if (!packets_rec)
-            continue;
-
-        if (!start) {
-            start = 1;
-            alarm(args.duration);
-        }
-        packetsReceived = packetsReceived + packets_rec;
-        for (int i = 0; i < packets_rec; i++) {
-            add_recv_request(socketfd, args.size);
-            struct request* req = io_uring_cqe_get_data(cqe[i]);
-
-            if(args.debug && (packets_rec==args.batching))
-                printf("Emptied queue\n");
+      for(int i=0;i<args.batching;i++)
+            add_recv_request(socketfd,args.size);
 
 
-            freemsg(req->message);
-            free(req);
-        }
-        io_uring_submit(&ring);
-        io_uring_cq_advance(&ring,packets_rec);
+      printf("Entering server loop\n");
+      while (1) {
+            io_uring_submit_and_wait(&ring,args.batching);
+            packets_rec = io_uring_peek_batch_cqe(&ring, cqe, args.batching);
 
-    }
+            if (!start) {
+                  start = 1;
+                  alarm(args.duration);
+            }
+
+            packetsReceived = packetsReceived + packets_rec;
+
+            for (int i = 0; i < packets_rec; i++) {
+                  add_recv_request(socketfd, args.size);
+                  struct request* req = io_uring_cqe_get_data(cqe[i]);
+
+                  if(args.debug && (packets_rec==args.batching))
+                        printf("Emptied queue\n");
+
+                  freemsg(req->message);
+                  free(req);
+            }
+            
+            io_uring_cq_advance(&ring,packets_rec);
+
+      }
 }
 
 void sig_handler(int signum){
@@ -170,16 +170,16 @@ void sig_handler(int signum){
 }
 
 int main(int argc, char *argv[]){
-    int socketfd;
+      int socketfd;
 
-    parseArgs(argc, argv);
-    signal(SIGALRM,sig_handler);
+      parseArgs(argc, argv);
+      signal(SIGALRM,sig_handler);
 
-    io_uring_queue_init(32768,&ring,0);
-    socketfd = openListeningSocket(args.port);
+      io_uring_queue_init(32768,&ring,0);
+      socketfd = openListeningSocket(args.port);
 
-    printf("starting batching standard server\n");
-    startBatchingServer(socketfd);
+      printf("starting batching standard server\n");
+      startBatchingServer(socketfd);
 
 
 }
