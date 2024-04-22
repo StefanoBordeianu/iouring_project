c25311135d2d48ee2466bef1db79d7981da16283
ebpf <bordeianu.stefano@gmai.com>
2024-04-22 12:09:26 +0200

    fix

--- benchmarking/io_uring/udp/standard/standard_batching.c
+++ benchmarking/io_uring/udp/standard/standard_batching.c
@@ -131,7 +131,7 @@ int add_recv_request(int socket, long readlength){
       req->message = msg;
       io_uring_prep_recvmsg(sqe,socket, msg,0);
       io_uring_sqe_set_data(sqe, req);
-      io_uring_sqe_set_flags(sqe,IOSQE_ASYNC);
+      //io_uring_sqe_set_flags(sqe,IOSQE_ASYNC);
       return 1;
 }
 
