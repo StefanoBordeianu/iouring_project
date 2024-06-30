9c8ec13610309a6c4a5e68dc3ab8757ba76fbb35
ebpf <bordeianu.stefano@gmai.com>
2024-07-01 00:43:32 +0200

    fixes

--- benchmarking/socket/forward/proxy_raw_lv2.c
+++ benchmarking/socket/forward/proxy_raw_lv2.c
@@ -42,6 +42,7 @@ int main(int argc, char *argv[]){
       int size = 64;
       struct sockaddr_ll bind_ll;
       struct ifreq if_idx;
+      struct ifreq if_mac;
       struct msghdr msghdr;
       struct iovec iov;
       int op = 1;
@@ -64,21 +65,22 @@ int main(int argc, char *argv[]){
             perror("socket");
             return 0;
       }
+
+
       memset(&if_idx, 0, sizeof(struct ifreq));
       strncpy(if_idx.ifr_name, interface ,9);
-
       if (ioctl(sockfd, SIOCGIFINDEX, &if_idx) < 0)
             perror("SIOCGIFINDEX");
+      memset(&if_mac, 0, sizeof(struct ifreq));
+
+      strncpy(if_mac.ifr_name, interface, 9);
+      if (ioctl(sockfd, SIOCGIFHWADDR, &if_mac) < 0)
+            perror("SIOCGIFHWADDR");
+
 
       bind_ll.sll_family = AF_PACKET;
       bind_ll.sll_protocol = htons(ETH_P_IP);
       bind_ll.sll_ifindex = if_idx.ifr_ifindex;
-
-
-//      if(bind(sockfd,(const struct sockaddr*)&listen_add,sizeof(listen_add)) < 0){
-//            perror("bind");
-//            return 0;
-//      }
 
       char buffer[size];
       struct ether_header *eh = (struct ether_header *) buffer;
@@ -118,15 +120,30 @@ int main(int argc, char *argv[]){
                    eh->ether_shost[4],
                    eh->ether_shost[5]);
 
+            eh->ether_dhost[0] = eh->ether_shost[0];
+            eh->ether_dhost[1] = eh->ether_shost[1];
+            eh->ether_dhost[2] = eh->ether_shost[2];
+            eh->ether_dhost[3] = eh->ether_shost[3];
+            eh->ether_dhost[4] = eh->ether_shost[4];
+            eh->ether_dhost[5] = eh->ether_shost[5];
+            eh->ether_shost[0] = ((uint8_t *)&if_mac.ifr_hwaddr.sa_data)[0];
+            eh->ether_shost[1] = ((uint8_t *)&if_mac.ifr_hwaddr.sa_data)[1];
+            eh->ether_shost[2] = ((uint8_t *)&if_mac.ifr_hwaddr.sa_data)[2];
+            eh->ether_shost[3] = ((uint8_t *)&if_mac.ifr_hwaddr.sa_data)[3];
+            eh->ether_shost[4] = ((uint8_t *)&if_mac.ifr_hwaddr.sa_data)[4];
+            eh->ether_shost[5] = ((uint8_t *)&if_mac.ifr_hwaddr.sa_data)[5];
+
+
+
             recv_add.sll_ifindex = if_idx.ifr_ifindex;
             recv_add.sll_protocol = htons(ETH_P_IP);
             recv_add.sll_halen = ETH_ALEN;
-            recv_add.sll_addr[5] = 0x9c;
-            recv_add.sll_addr[4] = 0xdc;
-            recv_add.sll_addr[3] = 0x71;
-            recv_add.sll_addr[2] = 0x5d;
-            recv_add.sll_addr[1] = 0x31;
-            recv_add.sll_addr[0] = 0xd1;
+            recv_add.sll_addr[0] = 0x9c;
+            recv_add.sll_addr[1] = 0xdc;
+            recv_add.sll_addr[2] = 0x71;
+            recv_add.sll_addr[3] = 0x5d;
+            recv_add.sll_addr[4] = 0x31;
+            recv_add.sll_addr[5] = 0xd1;
 
 
 
