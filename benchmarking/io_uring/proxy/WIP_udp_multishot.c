95cb13f7625ad7b87a3891b9aa02f51643d94137
ebpf <bordeianu.stefano@gmai.com>
2024-07-27 18:23:57 +0200

    fixes

--- benchmarking/io_uring/proxy/WIP_udp_multishot.c
+++ benchmarking/io_uring/proxy/WIP_udp_multishot.c
@@ -276,7 +276,7 @@ void add_recv_multishot(int socketfd) {
       req->msg = msghdr;
       req->type = EVENT_TYPE_RECV;
       req->socket = socketfd;
-      io_uring_prep_recvmsg_multishot(sqe,socketfd,&msg,0);
+      io_uring_prep_recvmsg_multishot(sqe,socketfd,msghdr,0);
       io_uring_sqe_set_flags(sqe,IOSQE_BUFFER_SELECT);
       io_uring_sqe_set_flags(sqe,IOSQE_FIXED_FILE);
       io_uring_sqe_set_data(sqe,req);
